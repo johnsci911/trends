f8bc01968489dbfb5e21453c479b6ab4ad8ff0cc
jkcachero <johnguitarizta@gmail.com>
2025-04-28 19:29:21 +0800

    Layout of screens and navigators

---
+++ App.jsx
@@ -0,0 +1,112 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import NewTweet from './screens/NewTweet';
+import TweetScreen from './screens/TweetScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import SearchScreen from './screens/SearchScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home1"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{
+          title: 'Notifications',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="notifications" size={size} color={color} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen name="Tab" component={TabNavigator}
+        options={{
+          headerShown: false,
+          title: ''
+        }}
+      />
+      <Stack.Screen name="New Tweet" component={NewTweet}
+        options={{
+          title: ''
+        }}
+      />
+      <Stack.Screen name="Tweet Screen" component={TweetScreen}
+        options={{
+          title: ''
+        }}
+      />
+      <Stack.Screen name="Profile Screen" component={ProfileScreen}
+        options={{
+          title: ''
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerShown: true,
+
+        }}
+      >
+        <Drawer.Screen name="Home" component={HomeStackNavigator} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
---
+++ screens/SearchScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SearchScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Search Screen</Text>
+    </View>
+  )
+}
+
+
