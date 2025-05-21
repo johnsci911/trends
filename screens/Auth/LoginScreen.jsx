aa069288659f4df9e1ead4d3a24e1de19ff96a3c
jkcachero <johnguitarizta@gmail.com>
2025-05-21 20:53:05 +0800

    Login screen

--- screens/Auth/LoginScreen.jsx
+++ screens/Auth/LoginScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { ActivityIndicator, Button, Text, TextInput, View } from "react-native";
+import { ActivityIndicator, Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { AuthContext } from '../../components/context/AuthProvider';
 
 export default function LoginScreen({ navigation }) {
@@ -8,32 +8,87 @@ export default function LoginScreen({ navigation }) {
   const { login, error, isLoading } = useContext(AuthContext);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <TextInput
-        onChangeText={setEmail}
-        value={email}
-        placeholder="Email"
-        placeholderTextColor="gray"
-        textContentType='emailAddress'
-        keyboardType='email-address'
-        autoCapitalize='none'
-      />
-      <TextInput
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Password"
-        placeholderTextColor="gray"
-        autoCapitalize='none'
-        secureTextEntry={true}
-      />
-      <Button onPress={() => login(email, password)} title="Login" />
-      <Button onPress={() => navigation.navigate('Register Screen')} title="Go to Register Page" />
+    <View style={styles.container}>
+      <View style={{ marginTop: 130, width: 260 }}>
+        <View style={{ alignItems: 'center' }}>
+          <Image style={styles.logo} source={require('../../assets/larydefault.png')}></Image>
+        </View>
+        <View style={{ marginTop: 40 }}>
+          {error && <Text style={{ color: 'red' }}>{error}</Text>}
+          <TextInput
+            style={[styles.inputBox, styles.mt4]}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Email"
+            placeholderTextColor="gray"
+            textContentType='emailAddress'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+          <TextInput
+            style={[styles.inputBox, styles.mt4]}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Password"
+            placeholderTextColor="gray"
+            autoCapitalize='none'
+            secureTextEntry={true}
+          />
+        </View>
+        <TouchableOpacity onPress={() => login(email, password)} style={[styles.loginButton, styles.mt5]}>
+          {isLoading && (
+            <ActivityIndicator size="small" style={{ marginRight: 18 }} color="white" />
+          )}
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
 
-      {error && <Text style={{ color: 'red' }}>{error}</Text>}
-      {isLoading && (
-        <ActivityIndicator size="large" color="gray" />
-      )}
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 12 }}>
+          <Text style={styles.registerText}>Do you have an account? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Register Screen')}>
+            <Text style={styles.registerTextLink}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1d9bf1',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 120,
+  },
+  inputBox: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+  },
+  mt4: {
+    marginTop: 16,
+  },
+  mt5: {
+    marginTop: 22,
+  },
+  loginButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0084b3',
+    padding: 12,
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    color: 'white',
+  },
+  registerTextLink: {
+    color: 'white',
+    fontSize: 12,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  }
+})
